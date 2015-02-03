54a11ec6ac1ca2fc3adf4b7481302d23ee2240f9
Jimmy Tsao <jimmy.tsao@gmail.com>
2015-02-02 21:21:44 -0600

    (feat) - add ability to take pictures with cordova camera plugin

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,49 @@
-// Ionic Starter App
+'use strict';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', [
-  'ionic',
-  'ngCordova'])
+angular
+  .module('starter', [
+    'ionic',
+    'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .controller('imageController', function($scope, $ionicPlatform, $cordovaCamera){
+
+    $scope.dataUrl;
+
+    $ionicPlatform.ready(function(){
+
+      $scope.takePicture = function takePicture(){
+        
+        var options = {
+          quality: 100,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: false,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 1000,
+          targetHeight: 1000,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+          $scope.dataUrl = imageData;
+        }, function(err) {
+          // error
+          console.log('ERROR: ', err);
+        });      
+      };
+    });
+  })
